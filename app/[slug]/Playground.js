b0fb0275cb5208426db82fca01b0c5f209759142
nguyenyou <tunt081295@gmail.com>
2025-06-07 22:59:23 +0700

    🚢 ship it

--- app/[slug]/Playground.js
+++ app/[slug]/Playground.js
@@ -1,5 +1,4 @@
-import * as React from "react";
-import { Tabs } from "@base-ui-components/react/tabs";
+import { FileExplorer } from "./FileExplorer";
 import PreviewContainer from "./PreviewContainer";
 import { ReactPlayground } from "./ReactPlayground";
 import { TailwindPlayground } from "./TailwindPlayground";
@@ -43,45 +42,7 @@ export const Playground = (props) => {
 
   return (
     <div>
-      <Tabs.Root
-        className="rounded-md border border-gray-200"
-        defaultValue="index.html"
-      >
-        <Tabs.List className="relative z-0 flex gap-1 px-1 shadow-[inset_0_-1px] shadow-gray-200">
-          <Tabs.Tab
-            className="flex h-8 items-center justify-center border-0 px-2 text-sm font-medium text-gray-600 outline-none select-none before:inset-x-0 before:inset-y-1 before:rounded-sm before:-outline-offset-1 before:outline-blue-800 hover:text-gray-900 focus-visible:relative focus-visible:before:absolute focus-visible:before:outline-2 data-[selected]:text-gray-900"
-            value="index.html"
-          >
-            index.html
-          </Tabs.Tab>
-          <Tabs.Tab
-            className="flex h-8 items-center justify-center border-0 px-2 text-sm font-medium text-gray-600 outline-none select-none before:inset-x-0 before:inset-y-1 before:rounded-sm before:-outline-offset-1 before:outline-blue-800 hover:text-gray-900 focus-visible:relative focus-visible:before:absolute focus-visible:before:outline-2 data-[selected]:text-gray-900"
-            value="styles.css"
-          >
-            styles.css
-          </Tabs.Tab>
-          <Tabs.Tab
-            className="flex h-8 items-center justify-center border-0 px-2 text-sm font-medium text-gray-600 outline-none select-none before:inset-x-0 before:inset-y-1 before:rounded-sm before:-outline-offset-1 before:outline-blue-800 hover:text-gray-900 focus-visible:relative focus-visible:before:absolute focus-visible:before:outline-2 data-[selected]:text-gray-900"
-            value="index.js"
-          >
-            index.js
-          </Tabs.Tab>
-          <Tabs.Indicator className="absolute top-1/2 left-0 z-[-1] h-6 w-[var(--active-tab-width)] -translate-y-1/2 translate-x-[var(--active-tab-left)] rounded-sm bg-gray-100 transition-all duration-200 ease-in-out" />
-        </Tabs.List>
-        <Tabs.Panel className="h-32" value="index.html">
-          <pre className="h-full overflow-auto">
-            {files["/index.html"]?.code}
-          </pre>
-        </Tabs.Panel>
-        <Tabs.Panel className="h-32" value="styles.css">
-          <pre className="h-full overflow-auto">
-            {files["/styles.css"]?.code}
-          </pre>
-        </Tabs.Panel>
-        <Tabs.Panel className="h-32" value="index.js">
-          <pre className="h-full overflow-auto">{files["/index.js"]?.code}</pre>
-        </Tabs.Panel>
-      </Tabs.Root>
+      <FileExplorer files={files} />
       <PreviewContainer>
         <iframe
           srcDoc={srcDoc}
