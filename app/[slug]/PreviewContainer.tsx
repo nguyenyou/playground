ecc6e111ad59b7f909ca00499e5452251f857bc8
nguyenyou <tunt081295@gmail.com>
2025-06-08 13:46:35 +0700

    🚢 ship it

--- app/[slug]/PreviewContainer.tsx
+++ app/[slug]/PreviewContainer.tsx
@@ -59,7 +59,7 @@ export default function PreviewContainer({ children, minWidth = 200 }: Props) {
 
   return (
     <div className="flex flex-col gap-1  bg-gray-950/5 p-1 inset-ring inset-ring-gray-950/5">
-      <div ref={containerRef} className="relative w-full bg-white h-full">
+      <div ref={containerRef} className="relative w-full bg-white h-full overflow-hidden rounded-md">
         {children}
         <div
           className="pointer-events-auto absolute top-1/2 right-2 z-50 -mt-6 h-12 w-1.5 cursor-ew-resize rounded-full bg-slate-950/20 hover:bg-slate-950/40"
